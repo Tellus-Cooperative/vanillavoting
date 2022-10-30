dd79a50f6b0996992a199a59071f54ae9e7767ef
nate-d3v <nate.d3v@gmail.com>
2022-10-30 12:21:08 -0300

    Connect account to stellar horizon api

--- app/routes/proposals/user.tsx
+++ app/routes/proposals/user.tsx
@@ -4,17 +4,39 @@ import { getUser } from '~/utils/session.server';
 
 export const loader: LoaderFunction = async ({ request }) => {
 	const userKey = await getUser(request);
-	const data = { userKey };
+	const stellarAccount = await fetch(
+		'https://horizon.stellar.org/accounts/GCTLGFMM2KFVGJDBVAWYCFK3P22MNG2O3MS6K6LCH5CLPW45TVCJSZU2' //+ userKey
+	);
+	const stellarAccountData = await stellarAccount.json();
+	const stellarClaimable = await fetch(
+		'https://horizon.stellar.org/claimable_balances?claimant=GCTLGFMM2KFVGJDBVAWYCFK3P22MNG2O3MS6K6LCH5CLPW45TVCJSZU2'
+	); //+ userKey
+	const stellarClaimableData = await stellarClaimable.json();
 
 	if (!userKey) {
 		return redirect('/proposals');
 	}
 
-	return data;
+	return { userKey, stellarAccountData, stellarClaimableData };
 };
 
 export default function User() {
-	const { userKey } = useLoaderData();
+	const { userKey, stellarAccountData, stellarClaimableData } = useLoaderData();
+
+	const xlm = stellarAccountData.balances.find(
+		(item: any) => item.asset_type === 'native'
+	);
+	const coop = stellarAccountData.balances.find(
+		(item: any) => item.asset_code === 'COOP'
+	);
+	const signers = stellarAccountData.signers.map((item: any) => item);
+	const claimables = stellarClaimableData['_embedded'].records
+		.map((item: any) => [
+			item.id,
+			item.asset.slice(0, item.asset.indexOf(':')),
+			Math.trunc(item.amount),
+		])
+		.sort();
 
 	return (
 		<div className="flex-1 flex flex-col py-5 px-10">
@@ -28,28 +50,41 @@ export default function User() {
 				</span>
 			</div>
 			<div className="flex space-x-2 min-h-[50vh]">
-				<div className="basis-2/3 border-black border-[3px] rounded p-7 bg-neutral-300">
+				<div className="basis-1/2 border-black border-[3px] rounded p-7 bg-neutral-300">
 					<div className="mb-8">
 						<h3 className="inline-block font-flex text-base font-bold mr-2">
 							Account Balance
 						</h3>
-						<span className="inline-block font-flex text-base">~ 4000 USD</span>
+						{/* <span className="inline-block font-flex text-base">~ 4000 USD</span> */}
 					</div>
-					<h4 className="font-flex text-base mb-2">XLM: 100000 ~ 100 USD</h4>
-					<h4 className="font-flex text-base mb-8">COOP: 100000 ~ 20 USD</h4>
-					<h3 className="font-flex text-base font-bold">
+					<h4 className="font-flex text-base mb-2">
+						XLM: {Math.trunc(xlm.balance)}
+						{/*  ~ 100 USD */}
+					</h4>
+					<h4 className="font-flex text-base mb-8">
+						COOP: {coop ? Math.trunc(coop.balance) : 0}
+						{/*  ~ 20 USD */}
+					</h4>
+					<h3 className="font-flex text-base font-bold mb-4">
 						Pending Claimable Balances
 					</h3>
+					<div className="max-h-[10rem] overflow-y-scroll">
+						{claimables.map((item: any) => (
+							<h4 key={item[0]} className="font-flex text-base mb-2">
+								{`${item[1]}: ${item[2]}`}
+							</h4>
+						))}
+					</div>
 				</div>
-				<div className="basis-1/3 border-black border-[3px] rounded p-7 bg-neutral-300">
-					<h3 className="font-flex text-base font-bold mb-1">Created</h3>
-					<h4 className="font-flex text-base mb-2">placeholder</h4>
-					<h3 className="font-flex text-base font-bold mb-1">Created by</h3>
-					<h4 className="font-flex text-base mb-2">placeholder</h4>
+				<div className="basis-1/2 border-black border-[3px] rounded p-7 bg-neutral-300">
 					<h3 className="font-flex text-base font-bold mb-1">
 						Account Signers
 					</h3>
-					<h4 className="font-flex text-base mb-2">placeholder</h4>
+					{signers.map((item: any) => (
+						<h4 key={item.key} className="font-flex text-base mb-2">
+							{item.key}
+						</h4>
+					))}
 				</div>
 			</div>
 		</div>
